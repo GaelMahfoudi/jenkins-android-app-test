477b43cc1aba96e99d6a54c726c2f26cde70887e
gael mahfoudi <gael.mahfoudi@airweb.de>
2016-04-20 17:04:30 +0200

    added another stupid test

---
+++ app/src/androidTest/java/gael/com/jenkinstest/StupidTest.java
@@ -0,0 +1,32 @@
+package gael.com.jenkinstest;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+
+/**
+ * Created by aurelien on 20/04/16.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class StupidTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> test = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testClickOnTheFabForAbsolutlyNoReasonIndeedItWontDoAThingAnyway() {
+        onView(withId(R.id.fab)).perform(click());
+        onView(withId(R.id.fab)).check(matches(isDisplayed()));
+    }
+
+}
