9925c2dd997070f7da97f95239224cb7ca68ed79
gael mahfoudi <gael.mahfoudi@airweb.de>
2016-04-28 10:31:14 +0200

    new branch created

--- app/src/main/java/gael/com/jenkinstest/StupidClass.java
+++ app/src/main/java/gael/com/jenkinstest/StupidClass.java
@@ -4,7 +4,7 @@ package gael.com.jenkinstest;
  * Created by aurelien on 20/04/16.
  */
 public class StupidClass {
-    //Test
+    //Jean
     public static int returnTwo() {
         return 2;
     }
